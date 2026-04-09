3dd4be584872b54e5c306f50820fc842c17d65f8
gabi-abn <x9h8sy24cg@privaterelay.appleid.com>
2026-04-09 21:58:30 +0800

    Update views.py

---
+++ M6E-DjangoForms-AgbayaniBejasaNery/tapas/tapasapp/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render, redirect
+from .models import Account
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username', '').strip()
+        password = request.POST.get('password', '').strip()
+
+        account = Account.objects.filter(username=username, password=password).first()
+        if account:
+            return redirect('view_supplier')
+        else:
+            messages.error(request, 'Invalid login')
+    return render(request, 'tapas/login.html')
+
+def signup_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username', '').strip()
+        password = request.POST.get('password', '').strip()
+
+        if Account.objects.filter(username=username).exists():
+            messages.error(request, 'Account already exists')
+            return render(request, 'tapas/signup.html')
+        else:
+            Account.objects.create(username=username, password=password)
+            messages.success(request, 'Account created successfully')
+            return redirect('login')
+
+    return render(request, 'tapas/signup.html')
