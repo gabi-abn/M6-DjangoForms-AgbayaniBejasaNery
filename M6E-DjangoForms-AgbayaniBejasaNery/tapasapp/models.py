bf8ca02bafb3e04d992e754fec8ec945542570a9
gabi-abn <x9h8sy24cg@privaterelay.appleid.com>
2026-04-14 18:58:00 +0800

    Update models.py

--- M6E-DjangoForms-AgbayaniBejasaNery/tapasapp/models.py
+++ M6E-DjangoForms-AgbayaniBejasaNery/tapasapp/models.py
@@ -10,4 +10,17 @@ class Dish(models.Model):
 
     def __str__(self):
         return str(self.pk) + ": " + self.name
+
+class Account(models.Model):
+    username = models.CharField(max_length=150, unique=True)
+    password = models.CharField(max_length=128) 
+
+    def getUsername(self):
+        return self.username
+
+    def getPassword(self):
+        return self.password
+
+    def __str__(self):
+        return self.username
     
