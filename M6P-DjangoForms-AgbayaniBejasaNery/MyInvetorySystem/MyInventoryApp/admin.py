9737d96deae33469007ef48e1baed3aae7dbc329
Athan Nery <athan.vergel.nery@student.ateneo.edu>
2026-04-15 09:46:18 +0800

    small changes

--- M6P-DjangoForms-AgbayaniBejasaNery/MyInvetorySystem/MyInventoryApp/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Supplier, WaterBottle
-
-# Register your models here.
-admin.site.register(Supplier)
-admin.site.register(WaterBottle)
-
-
--- M6P-DjangoForms-AgbayaniBejasaNery/MyInvetorySystem/MyInventoryApp/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class MyinventoryappConfig(AppConfig):
-    name = 'MyInventoryApp'
